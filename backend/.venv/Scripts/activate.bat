6eb910d6a97f0005f47c448327f411c8834bf69b
Illya <illya.cheb@gmail.com>
2025-11-22 15:42:01 -0800

    small change

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\illya\460Project\campus-clubs-node-fastapi-sqlite\backend\.venv
+set VIRTUAL_ENV=C:\Users\illya\460Project\campus-club-activity-DEMO\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
